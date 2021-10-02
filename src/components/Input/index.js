7fead5a2038f9807d7f6c86ce64a166e2d3f5d0c
osemudiamen Elebesunu <eronscode@gmail.com>
2021-10-02 23:16:55 +0100

    feats: adds input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,60 @@
+import { forwardRef } from "react";
+import PropTypes from "prop-types";
+
+import { StyledInput, InputElement, ErrorContainer, InputWrapper } from "./styles";
+import { ErrorIcon } from "utils/icons";
+import Tooltip from "components/Tooltip";
+
+const propTypes = {
+  className: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  label: PropTypes.string,
+  isError: PropTypes.string,
+  errorMsg: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+const defaultProps = {
+  className: undefined,
+  name: undefined,
+  value: undefined,
+  label: undefined,
+  isError: undefined,
+  errorMsg: undefined,
+  onChange: () => {},
+};
+
+const Input = forwardRef(
+  (
+    { className, label, name, isError, errorMsg, onChange, value, ...inputProps },
+    ref
+  ) => {
+    return (
+      <InputWrapper>
+        {label && <label htmlFor={name}>{label} </label>}
+        <StyledInput className={className}>
+          {errorMsg && (
+            <ErrorContainer>
+              <Tooltip value={errorMsg} />
+              <ErrorIcon />
+            </ErrorContainer>
+          )}
+          <InputElement
+            value={value}
+            invalid={isError}
+            name={name}
+            ref={ref}
+            onChange={onChange}
+            {...inputProps}
+          />
+        </StyledInput>
+      </InputWrapper>
+    );
+  }
+);
+
+Input.propTypes = propTypes;
+Input.defaultProps = defaultProps;
+
+export default Input;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,67 @@
+import styled, { css } from "styled-components";
+
+import { color, font } from "styles/styleUtils";
+
+const InputWrapper = styled.div`
+  label {
+    ${font.bold};
+    ${font.size(12)};
+    line-height: 14px;
+    margin-bottom: 4px;
+    display: block;
+  }
+`;
+const StyledInput = styled.div`
+  position: relative;
+  display: inline-block;
+  width: 100%;
+  label {
+    ${font.bold};
+    ${font.size(12)};
+    line-height: 14px;
+    margin-bottom: 4px;
+    display: block;
+  }
+`;
+
+const InputElement = styled.input`
+  height: 100%;
+  width: 100%;
+  padding: 13px 10px;
+  height: 40px;
+  border: 1px solid ${color.lightGrey};
+  color: ${color.black};
+  background: ${color.white};
+  transition: background 0.1s;
+  ${font.size(12)};
+  line-height: 14px;
+
+  &:hover {
+    /* background: ${color.backgroundLight}; */
+  }
+  &:focus {
+    background: #fff;
+    border: 1px solid ${color.blue};
+    box-shadow: 0px 0px 8px rgba(0, 133, 255, 0.3);
+  }
+  ${(props) =>
+    props.invalid &&
+    css`
+      border: 1px solid ${color.red};
+      &,
+      &:focus {
+        border: 1px solid ${color.red};
+        box-shadow: none;
+      }
+    `}
+`;
+
+const ErrorContainer = styled.div`
+  position: absolute;
+  right: -106px;
+  top: -60px;
+  width: 200px;
+  text-align: center;
+`;
+
+export { InputElement, StyledInput, ErrorContainer , InputWrapper};
