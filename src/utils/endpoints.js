f898f1331d6728ab71d3e50b18ef66e6fafa6194
osemudiamen Elebesunu <eronscode@gmail.com>
2021-10-03 14:15:10 +0100

    refactor: code cleanup

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,5 +1,3 @@
 export const ApiEndpoints = {
   SEARCH_REPO: (query="", page="", per_page="") => `search/repositories?q=${query}&page=${page}&per_page=${per_page}`,
-  //GET_ORG_REPOS: (org) => `repos/typescript-cheatsheets/react/contributors`,
-  //SEARCH_ORGS_REPOS_BY_KEYWORD: (query="", org="", per_page=100) => `search/repositories?q=${query} in:name,description+org:${org}&per_page=${per_page}`,
 };
